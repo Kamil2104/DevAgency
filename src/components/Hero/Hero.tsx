e8fef0dbf6d383bde101ee6e73d7ad3899779f0b
pindrive <kamilniewiadomski18@gmail.com>
2024-10-28 19:52:06 +0100

    feat: create and style Hero component (and make small changes for Header component)

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import BigLogo from './components/BigLogo'
+import ScrollDownSign from './components/ScrollDownSign'
+import TextContent from './components/TextContent'
+
+import BackgroundVideo from './assets/HeroBackgroundVideo.mp4'
+
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <video autoPlay loop muted playsInline className="heroVideo"> <source src={BackgroundVideo} type="video/mp4" /> Your browser does not support the video tag. </video>
+      <section className='heroContent'>
+        <section className='firstRow'>
+          <BigLogo />
+        </section>
+        <section className='secondRow'>
+          <TextContent />
+          <ScrollDownSign />
+        </section>
+      </section>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Hero/components/TextContent.tsx
@@ -0,0 +1,11 @@
+import './styles/TextContent.css'
+
+const TextContent = () => {
+  return (
+    <section className='textContent'>
+      <p>&lt; We are a AI development agency <br /> focused on bringing designs, <br /> brands, and products to life&nbsp;/&gt;</p>
+    </section>
+  )
+}
+
+export default TextContent

22cfb8bf6a87d8f68028593fbfc0272b85cc15a6
pindrive <kamilniewiadomski18@gmail.com>
2024-10-29 19:58:42 +0100

    fix: repair warnings connected to svg's

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<{ text: string, stroke: string}> = ({ text, stroke }) => 
     <button className='originalBtn'>
         {text}
         <svg width="24" height="24" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5 12.1758L19 12.1758M19 12.1758L13 6.17578M19 12.1758L13 18.1758" stroke={stroke} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M5 12.1758L19 12.1758M19 12.1758L13 6.17578M19 12.1758L13 18.1758" stroke={stroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     </button>
   )
--- src/pages/HomePage/Boosts/components/Cards.tsx
+++ src/pages/HomePage/Boosts/components/Cards.tsx
@@ -20,7 +20,7 @@ const Card: React.FC<CardProps> = ({ card }) => {
     <section className='card'>
       <section className='cardHeader'>
         <svg width="28" height="28" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 20.1758L12 3.17578M12 3.17578L5 10.4615M12 3.17578L19 10.4615" stroke="#F4F4F4" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M12 20.1758L12 3.17578M12 3.17578L5 10.4615M12 3.17578L19 10.4615" stroke="#F4F4F4" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
         <h4 className='cardPercent'> {card.percent}&nbsp; </h4>
         <h4 className='cardHeader'> {card.title} </h4>

33a7eaaaed1f8b3f0d0f9e9b16a9f06963f09862
pindrive <kamilniewiadomski18@gmail.com>
2024-10-31 15:20:18 +0100

    feat: add functionality for displaying form also when GetInTouch button is clicked

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<{ text: string, stroke: string}> = ({ text, stroke }) => 
   return (
     <button
       className='originalBtn'
-      onClick={() => text === "Let's talk" ? setIsFormDisplayed(true) : console.log('Nothing is here')}>
+      onClick={() => text === "Let's talk" || text === "Get in touch" ? setIsFormDisplayed(true) : console.log('Nothing is here')}>
         {text}
         <svg width="24" height="24" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M5 12.1758L19 12.1758M19 12.1758L13 6.17578M19 12.1758L13 18.1758" stroke={stroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>

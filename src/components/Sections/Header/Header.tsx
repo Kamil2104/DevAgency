a1257f1582fb4160bc16b821291851b464190e03
pindrive <kamilniewiadomski18@gmail.com>
2024-11-07 15:38:39 +0100

    feat: add functionality for showing header when scrolling up, when scrolling down it hides

--- src/components/Sections/Header/Header.tsx
+++ src/components/Sections/Header/Header.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { useFormContext } from '../../../hooks/useFormContext'
@@ -13,21 +12,41 @@ import './Header.css'
 
 const Header: React.FC = () => {
   const navigate = useNavigate()
-
   const { setIsFormDisplayed } = useFormContext()
 
+  const [isVisible, setIsVisible] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(window.scrollY)
+
   const navbarOptions: NavbarOption[] = [
-    { text: "Our products", type: "text", onClick: () => { }},
+    { text: "Our products", type: "text", onClick: () => {} },
     { text: "About us", type: "text", onClick: () => { navigate('/AboutUs') } },
     { text: "Let's talk", type: "button", onClick: () => { setIsFormDisplayed(true) } },
   ]
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setIsVisible(false)
+      } else {
+        setIsVisible(true)
+      }
+
+      setLastScrollY(currentScrollY)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [lastScrollY])
+
   return (
-    <header>
+    <header className={`header ${isVisible ? "visible" : "hidden"}`}>
       <Logo />
       <Navbar navbarOptions={navbarOptions} />
     </header>
   )
 }
 
-export default Header;
+export default Header

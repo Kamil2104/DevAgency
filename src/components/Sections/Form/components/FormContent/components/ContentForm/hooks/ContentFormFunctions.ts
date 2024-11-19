e6254c17edb3c73b4bf240033bd4fab79a62a217
pindrive <kamilniewiadomski18@gmail.com>
2024-11-19 13:52:47 +0100

    feat: use useCallback and useMemo in ContentFormFunctions custom hook

--- src/components/Sections/Form/components/FormContent/components/ContentForm/hooks/ContentFormFunctions.ts
+++ src/components/Sections/Form/components/FormContent/components/ContentForm/hooks/ContentFormFunctions.ts
@@ -1,81 +1,81 @@
-import useFormContext from "../../../../../../../../hooks/useFormContext"
+import { useCallback, useMemo } from "react";
 
-import { isUserDataCorrectlyFulfilled } from "../../validation/validateFormUserData"
+import useFormContext from "../../../../../../../../hooks/useFormContext";
+
+import { isUserDataCorrectlyFulfilled } from "../../validation/validateFormUserData";
 
 import { UserData } from "../../hooks/useFormUserData";
 import { UserDataErrors } from "../../hooks/useFormUserDataErrors";
 import { UserDataSelectedProducts } from "../../hooks/useFormUserSelectedProducts";
 
 interface ContentFormFunctionsProps {
-    userData: UserData,
-    setUserData: React.Dispatch<React.SetStateAction<UserData>>,
-    userDataErrors: UserDataErrors,
-    setUserDataErrors: React.Dispatch<React.SetStateAction<UserDataErrors>>,
-    userSelectedProducts: UserDataSelectedProducts,
-    setUserSelectedProducts: React.Dispatch<React.SetStateAction<UserDataSelectedProducts>>,
-    isProductRequirementsSelectOpened: boolean,
-    setIsProductRequirementsSelectOpened: (isOpened: boolean) => void,
+  userData: UserData,
+  setUserData: React.Dispatch<React.SetStateAction<UserData>>,
+  userDataErrors: UserDataErrors,
+  setUserDataErrors: React.Dispatch<React.SetStateAction<UserDataErrors>>,
+  userSelectedProducts: UserDataSelectedProducts,
+  setUserSelectedProducts: React.Dispatch<React.SetStateAction<UserDataSelectedProducts>>,
+  isProductRequirementsSelectOpened: boolean,
+  setIsProductRequirementsSelectOpened: (isOpened: boolean) => void;
 }
 
-const useContentFormFunctions = ({ userData, setUserData, userDataErrors,  setUserDataErrors, userSelectedProducts, setUserSelectedProducts, isProductRequirementsSelectOpened, setIsProductRequirementsSelectOpened }: ContentFormFunctionsProps) => {
-    const { setDisplayedFormContent } = useFormContext()
+const useContentFormFunctions = ({ userData, setUserData, userDataErrors, setUserDataErrors, userSelectedProducts, setUserSelectedProducts, isProductRequirementsSelectOpened, setIsProductRequirementsSelectOpened }: ContentFormFunctionsProps) => {
+  const { setDisplayedFormContent } = useFormContext();
 
-    // Function for handling value changing in input fields
-    const handleValueChangeInFormField = (userDataName: string, newUserDataValue: string, userDataErrorName: string, inputFieldID: string) => {
-        setUserData({ ...userData, [userDataName]: newUserDataValue })
-        setUserDataErrors({...userDataErrors, [userDataErrorName]: '', formError: '' })
+  const handleValueChangeInFormField = useCallback((userDataName: string, newUserDataValue: string, userDataErrorName: string, inputFieldID: string) => {
+      setUserData({ ...userData, [userDataName]: newUserDataValue });
+      setUserDataErrors({ ...userDataErrors, [userDataErrorName]: "", formError: "" });
 
-        const inputField = document.getElementById(inputFieldID);
-        if (inputField) {
-          inputField.style.borderColor = '#131313';
-        }
+      const inputField = document.getElementById(inputFieldID);
+      if (inputField) {
+        inputField.style.borderColor = "#131313";
       }
+  }, [userData, userDataErrors, setUserData, setUserDataErrors]);
 
-      // Functions for product requirements
-      const handleSelectingProductRequirements = (selectedUserProduct: string, newSelectedUserProduct: string) => {
-        setUserSelectedProducts({...userSelectedProducts, [selectedUserProduct]: true})
-        setUserData({
-          ...userData,
-          productRequirements: [...userData.productRequirements, newSelectedUserProduct],
-        });
-        setUserDataErrors({...userDataErrors, formError: '' })
+
+  const handleSelectingProductRequirements = useCallback((selectedUserProduct: string, newSelectedUserProduct: string) => {
+      setUserSelectedProducts({ ...userSelectedProducts, [selectedUserProduct]: true });
+      setUserData({ ...userData, productRequirements: [ ...userData.productRequirements, newSelectedUserProduct ] });
+      setUserDataErrors({ ...userDataErrors, formError: "" });
+  }, [ userData, userDataErrors, userSelectedProducts, setUserSelectedProducts, setUserData, setUserDataErrors ]);
+
+
+  const handleDeselectingProductRequirements = useCallback((selectedUserProduct: string, deselectedUserProduct: string) => {
+      setUserSelectedProducts({ ...userSelectedProducts, [selectedUserProduct]: false });
+      setUserData((prevUserData) => ({ ...prevUserData, productRequirements: prevUserData.productRequirements.filter((requirement) => requirement !== deselectedUserProduct)}));
+  }, [userSelectedProducts, setUserSelectedProducts, setUserData]);
+
+
+  const handleProductRequirementsSelectToggle = useCallback(() => {
+      setIsProductRequirementsSelectOpened(!isProductRequirementsSelectOpened);
+  }, [isProductRequirementsSelectOpened, setIsProductRequirementsSelectOpened]);
+
+
+  const handleClosingProductRequirementsSelect = useCallback(() => {
+      if (isProductRequirementsSelectOpened) {
+        setIsProductRequirementsSelectOpened(false);
       }
+  }, [isProductRequirementsSelectOpened, setIsProductRequirementsSelectOpened]);
 
-      const handleDeselectingProductRequirements = (selectedUserProduct: string, deselectedUserProduct: string) => {
-        setUserSelectedProducts({...userSelectedProducts, [selectedUserProduct]: false})
-        setUserData((prevUserData) => ({
-          ...prevUserData,
-          productRequirements: prevUserData.productRequirements.filter((requirement: string) => requirement !== deselectedUserProduct)
-        }))
+
+  const handleFormSubmition = useCallback(() => {
+      handleClosingProductRequirementsSelect();
+
+      if (isUserDataCorrectlyFulfilled(userData, setUserDataErrors)) {
+        setDisplayedFormContent("ThanksToTheUser");
       }
+  }, [ userData, setUserDataErrors, handleClosingProductRequirementsSelect, setDisplayedFormContent ]);
 
-      const handleProductRequirementsSelectToggle = () => {
-        setIsProductRequirementsSelectOpened(!isProductRequirementsSelectOpened)
-      }
+  return useMemo(
+    () => ({
+      handleValueChangeInFormField,
+      handleSelectingProductRequirements,
+      handleDeselectingProductRequirements,
+      handleProductRequirementsSelectToggle,
+      handleClosingProductRequirementsSelect,
+      handleFormSubmition,
+    }), [ handleValueChangeInFormField, handleSelectingProductRequirements, handleDeselectingProductRequirements, handleProductRequirementsSelectToggle, handleClosingProductRequirementsSelect, handleFormSubmition, ]
+  );
+};
 
-      const handleClosingProductRequirementsSelect = () => {
-        if (isProductRequirementsSelectOpened) {
-          setIsProductRequirementsSelectOpened(false)
-        }
-      }
-
-      // Function for form validation and submition
-      const handleFormSubmition = () => {
-        handleClosingProductRequirementsSelect()
-
-        if (isUserDataCorrectlyFulfilled(userData, setUserDataErrors)) {
-          setDisplayedFormContent('ThanksToTheUser')
-        }
-      }
-
-    return {
-        handleValueChangeInFormField,
-        handleSelectingProductRequirements,
-        handleDeselectingProductRequirements,
-        handleProductRequirementsSelectToggle,
-        handleClosingProductRequirementsSelect,
-        handleFormSubmition
-    }
-}
-
-export default useContentFormFunctions
+export default useContentFormFunctions;

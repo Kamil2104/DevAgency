6bde901b9bc74171fdc63e4caab38faf2e9c3a49
pindrive <kamilniewiadomski18@gmail.com>
2024-11-25 20:51:01 +0100

    feat: add animation for arrow svg's in hero component and footer component

--- src/components/Sections/Footer/components/Arrow.tsx
+++ src/components/Sections/Footer/components/Arrow.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./styles/Arrow.css";
 
 const Arrow: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   const scrollToTop = () => {
     const startPosition = window.scrollY;
     const targetPosition = 0;
@@ -30,7 +31,11 @@ const Arrow: React.FC = () => {
   };
 
   return (
-    <section className="arrowIconContainer">
+    <section
+      className="arrowIconContainer"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <svg
         onClick={scrollToTop}
         width="123"
@@ -38,6 +43,7 @@ const Arrow: React.FC = () => {
         viewBox="0 0 123 123"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        className={`arrowIcon default ${isHovered ? "fadeOut" : "fadeIn"}`}
       >
         <rect
           x="0.5"
@@ -68,6 +74,32 @@ const Arrow: React.FC = () => {
             <stop offset="0.94" stopColor="#B8C1FF" />
           </linearGradient>
         </defs>
+      </svg>
+
+      <svg
+        onClick={scrollToTop}
+        width="123"
+        height="123"
+        viewBox="0 0 123 123"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={`arrowIcon hovered ${isHovered ? "fadeIn" : "fadeOut"}`}
+      >
+        <rect
+          x="0.5"
+          y="0.5"
+          width="122"
+          height="122"
+          rx="61"
+          stroke="#F4F4F4"
+        />
+        <path
+          d="M61 82.1819L61 40.8182M61 40.8182L43.5 58.5455M61 40.8182L78.5 58.5455"
+          stroke="#F4F4F4"
+          strokeWidth="3.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     </section>
   );

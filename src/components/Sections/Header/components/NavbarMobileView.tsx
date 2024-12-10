a35a453d442802470b3c514fe52917a026156b9b
pindrive <kamilniewiadomski18@gmail.com>
2024-12-10 23:25:18 +0100

    feat: enter font-family and font-color CSS variables and use them across the whole website

--- src/components/Sections/Header/components/NavbarMobileView.tsx
+++ src/components/Sections/Header/components/NavbarMobileView.tsx
@@ -4,8 +4,6 @@ import HamburgerIcon from './HamburgerIcon'
 
 import { NavbarMobileViewProps } from '../interfaces/interfaces'
 
-import './styles/NavbarMobileView.css'
-
 const NavbarMobileView: React.FC<NavbarMobileViewProps> = React.memo(({ toggleIsMenuOpen, displayedIcon, setDisplayedIcon }) => {
   return (
     <nav className='mobileView'>

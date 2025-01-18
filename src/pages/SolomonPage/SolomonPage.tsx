5ce3a8ea7ee85bde358be44b220f3c9a428285bc
Marcel Lademan <kamilniewiadomski18@gmail.com>
2025-01-18 23:24:20 +0100

    feat: create and style Comparison component

--- src/pages/SolomonPage/SolomonPage.tsx
+++ src/pages/SolomonPage/SolomonPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import HeaderHero from './HeaderHero/HeaderHero'
+import Comparison from './Comparison/Comparison'
 
 const SolomonPage: React.FC = () => {
   return (
     <section className='solomonPage'>
         <HeaderHero />
+        <Comparison />
     </section>
   )
 }

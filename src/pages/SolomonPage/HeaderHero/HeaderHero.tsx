67f2def582c5007ba8951a903d5a31fbb9b0ed63
pindrive <kamilniewiadomski18@gmail.com>
2025-01-29 14:44:50 +0100

    feat: add Solomon on desktop SVG on Hero section in Our product subpage

--- src/pages/SolomonPage/HeaderHero/HeaderHero.tsx
+++ src/pages/SolomonPage/HeaderHero/HeaderHero.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
+import { ReactSVG } from 'react-svg'
+
 import Header from '../../../components/Sections/Header/Header'
+
+import SolomonOnDesktop from './assets/Desktop - Solomon.svg'
 
 import './HeaderHero.css'
 
@@ -18,6 +22,7 @@ const Hero: React.FC = () => {
     <section className='hero'>
       <h1> Solomon </h1>
       <h3> An AI-powered legal tool that streamlines risk analysis, document summaries, and legal research. </h3>
+      <ReactSVG src={SolomonOnDesktop} className='solomonDesktop' />
     </section>
   )
 }
